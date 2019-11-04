d95126dcd11d638728c20cd4e562da248b90cc4a
Alvaro <alvaro.sebastian98@gmail.com>
2019-11-04 17:04:54 -0500

    Inserción inicial en el repositorio del módulo Trigger

---
+++ Trigger/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Trigger
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Item1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Data());
+
+            };
+            Item2.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Event());
+            };
+
+            Item4.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Property());
+
+            };
+            Item5.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Style());
+
+            };
+
+            Item6.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new MultiTrigger());
+
+            };
+
+        }
+
+    }
+}
---
+++ Trigger/Property.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Trigger
+{
+    public partial class Property : ContentPage
+    {
+        public Property()
+        {
+            InitializeComponent();
+        }
+    }
+}
